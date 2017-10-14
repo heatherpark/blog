dbbfe67c29f7fcbadf5a6a97af5cd2b5c6cf98cd
Heather Park <hthr.prk@gmail.com>
2017-10-14 15:58:33 -0700

    Render post content correctly

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -9,9 +9,17 @@ class PostsShow extends Component {
   }
 
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        Posts Show!
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
@@ -21,4 +29,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShows);
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
