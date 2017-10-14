669d330082dcc36b0027003508c7b861f1867063
Heather Park <hthr.prk@gmail.com>
2017-10-14 13:43:04 -0700

    Add other fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  renderTitleField(field) {
+  renderField(field) {
     return (
-      <div>
+      <div className="form-group">
+        <label>{field.label}</label>
         <input 
+          className="form-control"
           type="text"
           {...field.input}
         />
@@ -17,8 +19,20 @@ class PostsNew extends Component {
     return (
       <form>
         <Field
+          label="Title"
           name="title"
-          component={this.renderTitleField} />
+          component={this.renderField}
+        />
+        <Field 
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field 
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
       </form>
     );
   }
