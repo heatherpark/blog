dbb57fc92373787cf155d60be7f12bb952072660
Heather Park <hthr.prk@gmail.com>
2017-10-16 14:51:24 -0700

    Add comments to root index

--- src/index.js
+++ src/index.js
@@ -10,12 +10,16 @@ import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
 import PostsShow from './components/posts_show';
 
+// use redux-promise middleware to handle API requests
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
+  // Provider component makes Redux store available to all container components
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
+        {/* Switch allows for rendering a route exclusively
+        (only one of its children routes is rendered at any one time) */}
         <Switch>
           <Route path="/posts/new" component={PostsNew} />
           <Route path="/posts/:id" component={PostsShow} />
@@ -23,5 +27,5 @@ ReactDOM.render(
         </Switch>
       </div>
     </BrowserRouter>
-  </Provider>
-  , document.querySelector('.container'));
+  </Provider>,
+  document.querySelector('.container'));
