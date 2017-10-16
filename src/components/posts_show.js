3cc730b9b5f98263b1c75a8d650144314617be3f
Heather Park <hthr.prk@gmail.com>
2017-10-16 15:34:23 -0700

    Add comments to PostsShow

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -5,12 +5,15 @@ import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
+    // fetch post with id that matches ID param value in URL
     const { id } = this.props.match.params;
     this.props.fetchPost(id);
   }
 
   onDeleteClick() {
+    // delete post with id that matches ID param value in URL
     const { id } = this.props.match.params;
+    // pass callback to action creator to redirect after post deletion
     this.props.deletePost(id, () => {
       this.props.history.push('/');
     });
@@ -41,7 +44,11 @@ class PostsShow extends Component {
 }
 
 function mapStateToProps({ posts }, ownProps) {
+  // in 'posts' state, find post with key/id that matches id param value in URL
+  // pass as prop
   return { post: posts[ownProps.match.params.id] };
 }
 
+// connect Redux state to PostsIndex component by having Redux pass
+// necessary pieces of state and action creators as props
 export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
